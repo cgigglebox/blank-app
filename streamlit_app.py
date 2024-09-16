4c1a52e228cb94a8eeeebc87592430795b57a47e
cgigglebox <spirydovich@hotmail.com>
2024-09-16 21:45:20 +0000

    bob is the best

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 welcome Emory if you are here")
+st.title("🎈 welcome Emory if you are here or welcome ciggilbox if you are here")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
